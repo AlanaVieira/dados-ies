13b8094deea82fe43c0e4e443d21b37a6991fe93
Giana Kroth <gilucca@gmail.com>
2023-10-31 16:28:38 -0300

    arquivo de taes atualizados

--- Streamlit/pages/5_👩‍💻_Tecnicos_Administrativos.py
+++ Streamlit/pages/5_👩‍💻_Tecnicos_Administrativos.py
@@ -22,8 +22,16 @@ st.set_page_config(page_title='Analise TAE',
 
 st.title(':chart_with_upwards_trend: Força de trabalho técnico-adminsitrativa em educação :flag-br:')
 st.subheader('Escopo: Instituições de cursos Presenciais de Ensino Superior - Públicas e Privadas')
-st.subheader("Dados de 2012-2022")
-st.markdown("---")
+
+st.markdown('<p style="font-family:Courier; color:#992600; font-size: 20px;"><b>Dados de 2012 a 2022 </b></p>', unsafe_allow_html=True)    
+
+
+col1, col2 = st.columns(2)
+with col1:
+    label01 = '<p style="font-family:Courier; color:#992600; font-size: 20px;"><b>Selecione uma categoria:</b></p>'
+    st.markdown(label01, unsafe_allow_html=True)    
+with col2:
+    Categoria = st.selectbox("Selecione a categoria:", options=["Geral", "IES Públicas", "IES Privadas"], label_visibility="collapsed")
 # ------------------------------------------------------------------------
 # Parametros plots seaborn
 # ------------------------------------------------------------------------
@@ -36,14 +44,25 @@ sns.set(style="darkgrid")
 def carregar_dados_taes():
     data = pd.read_csv("../arquivos/dados_ies_taes_docentes_2012_2022.csv", low_memory=False)
     return data
-ies = carregar_dados_taes() 
+dados = carregar_dados_taes() 
 
-ies['QT_TEC_MASC'] = ies["QT_TEC_FUNDAMENTAL_INCOMP_MASC"] + ies["QT_TEC_FUNDAMENTAL_COMP_MASC"] + ies["QT_TEC_MEDIO_MASC"] + \
-    ies["QT_TEC_SUPERIOR_MASC"] + ies["QT_TEC_ESPECIALIZACAO_MASC"] + ies["QT_TEC_MESTRADO_MASC"] + ies["QT_TEC_DOUTORADO_MASC"] 
+dados['QT_TEC_MASC'] = dados["QT_TEC_FUNDAMENTAL_INCOMP_MASC"] + dados["QT_TEC_FUNDAMENTAL_COMP_MASC"] + dados["QT_TEC_MEDIO_MASC"] + \
+    dados["QT_TEC_SUPERIOR_MASC"] + dados["QT_TEC_ESPECIALIZACAO_MASC"] + dados["QT_TEC_MESTRADO_MASC"] + dados["QT_TEC_DOUTORADO_MASC"] 
 
-ies['QT_TEC_FEM'] =  ies["QT_TEC_FUNDAMENTAL_INCOMP_FEM"] + ies["QT_TEC_FUNDAMENTAL_COMP_FEM"]+ ies["QT_TEC_MEDIO_FEM"] + \
-    ies["QT_TEC_SUPERIOR_FEM"] + ies["QT_TEC_ESPECIALIZACAO_FEM"] + ies["QT_TEC_MESTRADO_FEM"] + ies["QT_TEC_DOUTORADO_FEM"] 
+dados['QT_TEC_FEM'] =  dados["QT_TEC_FUNDAMENTAL_INCOMP_FEM"] + dados["QT_TEC_FUNDAMENTAL_COMP_FEM"]+ dados["QT_TEC_MEDIO_FEM"] + \
+    dados["QT_TEC_SUPERIOR_FEM"] + dados["QT_TEC_ESPECIALIZACAO_FEM"] + dados["QT_TEC_MESTRADO_FEM"] + dados["QT_TEC_DOUTORADO_FEM"] 
 
+
+
+
+if Categoria == "Geral":
+    categorias_interesse = [1, 2, 3, 4, 5, 6]         
+elif Categoria == "IES Públicas":
+    categorias_interesse = [1, 2, 3]
+elif Categoria == "IES Privadas":
+    categorias_interesse = [4, 5, 6]
+
+ies = dados[dados['TP_CATEGORIA_ADMINISTRATIVA'].isin(categorias_interesse)]
 #-------------------------------------------------------------------------------------------------#
 # agrupar o dataframe para gerar o graficos por região e proporção de taes por aluno matriculado
 #-------------------------------------------------------------------------------------------------#
@@ -65,16 +84,12 @@ df_cat = df_cat.sort_values(by=(["NU_ANO_CENSO"]), ascending=False)
 #--------------------------------------------------------------------------#
 # agrupar o dataframe para gerar o graficos por sexo e tipo de instituição
 #--------------------------------------------------------------------------#
-df_sexo = ies.groupby(['NU_ANO_CENSO', 'TIPO_INST']).agg({'QT_TEC_MASC':'sum','QT_TEC_FEM':'sum'}).reset_index()
+df_sexo = ies.groupby(['NU_ANO_CENSO']).agg({'QT_TEC_MASC':'sum','QT_TEC_FEM':'sum'}).reset_index()
 
-df_sexo = df_sexo.rename(columns={'QT_TEC_MASC':'Masculino', 'QT_TEC_FEM':'Feminino'})
+df_sexo = df_sexo.rename(columns={'NU_ANO_CENSO':'Ano','QT_TEC_MASC':'Masculino', 'QT_TEC_FEM':'Feminino'})
 
-df_sexo = df_sexo.pivot(index='NU_ANO_CENSO', columns='TIPO_INST', values=['Masculino','Feminino']).reset_index()
-novas_colunas = ['Ano','Privada - Masculino', 'Pública - Masculino', 'Privada - Feminino', 'Pública - Feminino']
-df_sexo = df_sexo.set_axis(novas_colunas, axis=1)
-
-df_sexo = df_sexo.melt(id_vars='Ano', value_name='Total', var_name='Tipo/Sexo')
-df_sexo = df_sexo.sort_values(by=(["Ano", "Tipo/Sexo"]), ascending=False)
+df_sexo = df_sexo.melt(id_vars='Ano', value_name='Total', var_name='Sexo')
+df_sexo = df_sexo.sort_values(by=(["Ano"]), ascending=False)
 
 #--------------------------------------------------------------------#
 # Filtrar o data set somando as quantidades por escolaridade e sexo
@@ -105,6 +120,7 @@ df_escolaridade = df_escolaridade.groupby('NU_ANO_CENSO').agg({'Superior incompl
                                                   'Pós-Graduação - Feminino':'sum'}).reset_index()
 
 df_escolaridade_melt = df_escolaridade.melt(id_vars='NU_ANO_CENSO', var_name='Escolaridade', value_name='Total').dropna()
+
 
 
 col1,col2 = st.columns(2)    
@@ -157,8 +173,8 @@ col3,col4 = st.columns(2)
 with col3:    
     titulo_plot02 =  '<p style="font-family:Courier; color:Black; font-size: 23px;"><b>Técnicos Administrativos por Sexo e Tipo de Instituição - Evolução</b></p>'
     st.markdown(titulo_plot02, unsafe_allow_html=True)
-    fig1 = px.bar(df_sexo, y='Total', x='Ano', color='Tipo/Sexo', color_discrete_sequence=px.colors.qualitative.G10,
-                    barmode = 'stack', width=800, height=900, labels=dict(Tipo = 'Tipo/Sexo'), text='Total')
+    fig1 = px.bar(df_sexo, y='Total', x='Ano', color='Sexo', color_discrete_sequence=px.colors.qualitative.G10,
+                    barmode = 'stack', width=800, height=900, labels=dict(Tipo = 'Sexo'), text='Total')
     fig1.update_xaxes(tickangle=0)
     fig1.update_traces(textposition='inside', insidetextfont=dict(color='white', size=16,family='Times New Roman'))
     fig1.update_layout( yaxis=dict(title='', titlefont_size=22, tickfont_size=18),
@@ -180,3 +196,10 @@ with col4:
                        hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"))				  
     st.plotly_chart(fig3, use_container_width=True)	
     
+
+st.subheader("Principais Resultados")
+st.write("Ao analisarmos a série temporal que abrange o período de 2012 a 2022, percebemos que a região Sul é a que apresenta maior número de TAEs por alunos matriculados, impulsionado pelas Privadas.\
+    Entre as públicas a região Sudeste é a que apresenta maior número de TAEs/Aluno, porém em grande queda de 2016 a 2020. Dentre as privadas, as com fins lucrativos são as que apresentam o menor numero de TAES.")
+st.write("A relação entre homens e mulheres TAEs é bastante proporcional nas públicas, e nas privadas o número de homens é um pouco maior. Quanto à escolaridade, nas privadas o maior numero de TAEs são com superior incompleto, e dos que possuem formação superior as mulheres são maioria.\
+    Nas públicas as mulheres também se destacam pela maior escolaridade.")
+st.markdown("---")    
